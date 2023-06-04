7e578accd25a40a518060591128da885fe2c19f9
Eko Wijaya <ekowijayawindows@gmail.com>
2023-06-04 23:49:22 +0700

    update carousel untuk hero

--- src/components/HeroShayna.vue
+++ src/components/HeroShayna.vue
@@ -2,10 +2,10 @@
        <!-- Hero Section Begin -->
     <section class="hero-section">
         <carousel :items-to-show="1">
-            <slide v-for="slide in 10" :key="slide">
+            <slide v-for="slide in 3" :key="slide">
                 <div class="single-hero-items set-bg" style="background-image: url('img/hero-1.jpg')">
                 <div class="container">
-                    <div class="row">
+                    <div class="row banner-info">
                         <div class="col-lg-5">
                             <span>Bag,kids</span>
                             <h1>Black friday</h1>
@@ -17,7 +17,7 @@
                     </div>
                 </div>
             </div>
-                </slide>
+             </slide>
                 <template #addons>
         <navigation />
         <pagination />
@@ -43,3 +43,14 @@ export default {
     }
 };
 </script>
+
+<style>
+.set-bg{
+    width: 100%;
+    height: 500px;
+}
+
+.banner-info{
+    margin-top: 130px;
+}
+</style>
---
+++ src/views/Product.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="product">
+        <HeaderShayna/> 
+        <p>
+            Product Saya akan ada disini
+        </p>
+        <FooterShayna/>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+export default {
+  name: 'product',
+  components: {
+    HeaderShayna,
+    FooterShayna
+  }
+}
+</script>
