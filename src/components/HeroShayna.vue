7552123ac44b6b42164e320edb943411f671a1a8
Eko Wijaya <ekowijayawindows@gmail.com>
2023-06-04 16:45:17 +0700

    perbaikan carousel

---
+++ src/components/HeroShayna.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+       <!-- Hero Section Begin -->
+    <section class="hero-section">
+        <carousel class="hero-items" :items-to-show="1">
+            <slide v-for="slide in 10" :key="slide">
+                <div class="single-hero-items set-bg" data-setbg="img/hero-1.jpg">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-5">
+                            <span>Bag,kids</span>
+                            <h1>Black friday</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                            </p>
+                            <a href="#" class="primary-btn">Shop Now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                </slide>
+                <template #addons>
+        <navigation />
+        <pagination />
+      </template>
+
+          
+        </carousel>
+    </section>
+    <!-- Hero Section End -->
+    </div>
+</template>
+
+<script>
+
+import "vue3-carousel/dist/carousel.css";
+import {Carousel, Slide, Pagination, Navigation} from "vue3-carousel";
+
+
+export default {
+    name:"Heroshayna",
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation
+    }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
     <div class="home">
         <HeaderShayna/>
+
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeaderShayna from '@/components/HeaderShayna.vue';
+// import carousel from 'vue-owl-carousel';
 
 export default {
   name: 'Home',
